0431cf379e4e62b52faa84a25d523620efb7560a
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-28 10:15:21 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      usuario: '',
+      password: ''
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInput(e){
+    // 
+    const {value, name} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(e){
+    // Evitar que cambie el recurso
+    e.preventDefault()
+    console.log(this.state)
+    fetch("http://localhost:8081/usuario/agregarUsuario", {
+      method: 'POST',
+      body: JSON.stringify({
+        usuario: this.state.usuario,
+        password: this.state.password
+      }),
+      headers: {
+        'Content-Type': 'appication/json'
+      }
+    }).then(() => {
+      console.log('Lo logre')
+    })
+  }
+
+
+  render(){
+    console.log(this.state)
+    return(
+      <div className='App'>
+        <div className = 'card'>
+          <div className = 'card-header'>
+            <p>Esto es una prueba</p>
+          </div>
+          <div className = 'card-body'>
+            <form onSubmit={this.handleSubmit}>
+              <div className='form-group'>
+                <input type = "text" name = "usuario" onChange={this.handleInput} placeholder='Escribir usuario'/>
+              </div>
+              <div className='form-group'>
+                <input type = "password" name = "password" onChange={this.handleInput} placeholder='Escribir password'/>
+              </div>
+              <button type='submit'>Agregar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
